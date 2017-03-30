33f712a204645c26af91acf729371b15c34fb566
w0rmr1d3r <w0rmr1d3r@users.noreply.github.com>
2017-03-30 16:39:17 +0100

    checkstyle update

--- arbol.h
+++ arbol.h
@@ -5,23 +5,21 @@
 
 using namespace std;
 
-
 class arbol
 {
-public:
-    arbol(int _t);
-    nodo *root;
-    int t;    
-    void agregarLista(string nom);
-    Regalo agregargift(string regalo);
-    bool buscarKid(string nom);
-    int hash(string nom);
-    int fibonacci(int n, int back, int next, int cont);
-    int fibo(int n);
-    void traverse();
-    nodo* search(int k);
-    bool insert(string nom, int status, int longi, int lati, int edad);
-
+    public:
+        arbol(int _t);
+        nodo *root;
+        int t;    
+        void agregarLista(string nom);
+        Regalo agregargift(string regalo);
+        bool buscarKid(string nom);
+        int hash(string nom);
+        int fibonacci(int n, int back, int next, int cont);
+        int fibo(int n);
+        void traverse();
+        nodo* search(int k);
+        bool insert(string nom, int status, int longi, int lati, int edad);
 };
 
 #endif // ARBOL_H
